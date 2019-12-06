c1bf448d56806838475f97214b3c5c9a853167c0
Benjamin Kellermann <Benjamin.Kellermann@mgm-sp.com>
2019-12-06 09:00:50 +0100

    export as file download

--- www/export.php
+++ www/export.php
@@ -1,8 +1,12 @@
-<textarea>
 <?php
 $client = new SoapClient("http://quiz-api:8080/?wsdl");
 
-echo $client->exportAllQuestions()->return;
+if ( basename($_SERVER["SCRIPT_NAME"]) === "admin.php") {
+	header("Location: export.php");
+} else {
+	header("Content-type: application/json");
+	header('Content-disposition: inline; filename="questions.json"');
+	echo $client->exportAllQuestions()->return;
+}
 
 ?>
-</textarea>
